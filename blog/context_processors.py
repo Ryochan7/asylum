cf68793805b03cd83781e66060ae4e5f8e7e8fc2
Travis Nickles <ryoohki7@yahoo.com>
2011-07-27 01:54:57 -0500

    Added Disqus v4 javascript code

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,6 +4,7 @@ def paginate_setting (request):
     return {
         "paginate_by": settings.PAGINATE_BY,
         "ANALYTICS": settings.site_settings.ANALYTICS,
+        "DISQUS_SHORTNAME": settings.DISQUS_SHORTNAME,
         "DISQUS_DEVELOPER": settings.DISQUS_DEVELOPER,
     }
 
