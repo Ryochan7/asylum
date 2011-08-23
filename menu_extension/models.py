c28cdc4cb4455a851984523bc36c03b72ae534c5
Travis Nickles <ryoohki7@yahoo.com>
2011-08-23 00:44:13 -0500

    Altered CSS

--- menu_extension/models.py
+++ menu_extension/models.py
@@ -6,7 +6,7 @@ class MenuItemExtension(models.Model):
     menu_item = models.OneToOneField (MenuItem, related_name="extension")
     #published = models.BooleanField (default=True)
     protected = models.BooleanField (default=False)
-    css_classes = models.TextField (default="",
+    css_classes = models.TextField (blank=True, default="",
         help_text="Specify additional css classes to use for menu item"
     )
 
