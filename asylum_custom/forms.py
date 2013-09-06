6595518e03e99d7b017bb89ed93edfc33ae2f2d5
Travis Nickles <nickles.travis@gmail.com>
2013-09-06 11:59:08 -0500

    Fixed indentation in forms.py file

--- asylum_custom/forms.py
+++ asylum_custom/forms.py
@@ -4,16 +4,16 @@ from mezzanine.forms.forms import FormForForm
 
 class CustomContactForm(FormForForm):
     def clean_field_1(self):
-	name = self.cleaned_data["field_1"]
-	if not name.strip():
-	    raise forms.ValidationError("Please include a name")
+        name = self.cleaned_data["field_1"]
+        if not name.strip():
+            raise forms.ValidationError("Please include a name")
 
-	return name
+        return name
 
     def clean_field_4(self):
-	message = self.cleaned_data["field_4"]
-	if not message.strip():
-	    raise forms.ValidationError("Please specify a message")
+        message = self.cleaned_data["field_4"]
+        if not message.strip():
+            raise forms.ValidationError("Please specify a message")
 
-	return message
+        return message
 
