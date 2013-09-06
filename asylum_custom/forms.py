e2600a4088ecaa91153e26886252e97bdb67de5a
Travis Nickles <nickles.travis@gmail.com>
2013-09-06 11:55:50 -0500

    Override contact form validation to not allow empty strings in the contact form.

--- asylum_custom/forms.py
+++ asylum_custom/forms.py
@@ -1,1 +1,19 @@
+from django import forms
+
+from mezzanine.forms.forms import FormForForm
+
+class CustomContactForm(FormForForm):
+    def clean_field_1(self):
+	name = self.cleaned_data["field_1"]
+	if not name.strip():
+	    raise forms.ValidationError("Please include a name")
+
+	return name
+
+    def clean_field_4(self):
+	message = self.cleaned_data["field_4"]
+	if not message.strip():
+	    raise forms.ValidationError("Please specify a message")
+
+	return message
 
--- asylum_custom/page_processors.py
+++ asylum_custom/page_processors.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
-from mezzanine.core.views import direct_to_template
 from mezzanine.pages.page_processors import processor_for 
-from mezzanine.pages.models import Page
+from mezzanine.forms.models import Form
+from mezzanine.forms.signals import form_invalid, form_valid
+from .forms import CustomContactForm
 
-"""@processor_for("contact")
-def contact_page(request, page):
-    print "IN MINE"
-    return render(request, "contact.html", {"page": page})
-"""
+@processor_for(Form, "contact")
+def contact_form(request, page):
+    form = CustomContactForm(page.form, RequestContext(request),
+        request.POST or None, request.FILES or None)
+    
+    # If form is invalid, interrupt any other page processing
+    # and display errors found
+    if not form.is_valid():
+        form_invalid.send(sender=request, form=form)
+        return render(request, "pages/contact.html", {"page": page, "form": form})
+
+    return {}
 
