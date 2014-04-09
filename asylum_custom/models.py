5a6cd7cdf5c04caaaa28e7ac7ff427c54639b46e
Travis Nickles <nickles.travis@gmail.com>
2014-04-09 15:09:03 -0500

    Initial draft of email notification changes in DjangoBB.
    TODO: Possibly use a template for rendering the
    message string.

--- asylum_custom/models.py
+++ asylum_custom/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+
+
+from djangobb_forum.signals import post_saved, topic_saved
+from djangobb_forum.models import Post, Topic
+from .signals import post_saved as custom_post_saved
 
 # Create your models here.
+post_save.disconnect(receiver=post_saved, sender=Post)
+#post_save.disconnect(receiver=topic_saved, sender=Topic)
+
+post_save.connect(custom_post_saved, sender=Post, dispatch_uid='djangobb_post_save')
 
